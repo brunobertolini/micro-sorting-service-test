62805bec34aab94ca70f48a324f25da3ba240248
Bruno Bertolini <dev@brunobertolini.com>
2018-03-11 21:29:15 -0300

    feat: sorting method

--- app/sorting.js
+++ app/sorting.js
@@ -1,5 +1,25 @@
-function sorting(books) {
-	return books
+const normalize = item =>
+	typeof item === 'string' ? item.toLowerCase() : item
+
+const compare = (a, b) =>
+	normalize(a) < normalize(b) ? -1 : normalize(a) > normalize(b) ? 1 : 0
+
+const sortBy = props => {
+	const prop = props.shift()
+
+	return (a, b) => {
+		const sort = prop.reverse ? compare(b[prop.name], a[prop.name]) : compare(a[prop.name], b[prop.name])
+		return sort === 0 && props.length > 0 ? sortBy(props) : sort
+	}
 }
 
+const sorting = (data, props) => {
+	if (!props) {
+		throw new Error('Err')
+	} else if (props.length === 0) {
+		return []
+	} else {
+		return data.sort(sortBy(props))
+	}
+}
 module.exports = sorting
--- app/test.js
+++ app/test.js
@@ -1,35 +1,50 @@
 import test from 'ava'
 
 import books from './../data/books'
-import sort from './../app/sorting'
+import sorting from './../app/sorting'
 
 test('order by title asc', t => {
-	const result = sort(books, {title: 'asc'})
+	const props = [
+		{name: 'title'}
+	]
+
+	const result = sorting(books, props)
 	const ids = result.map(book => book.id)
 
 	t.deepEqual(ids, [1, 3, 4, 2])
 })
 
 test('order by: author asc and title desc', t => {
-	const result = sort(books, {author: 'asc', title: 'desc'})
+	const props = [
+		{name: 'author'},
+		{name: 'title', reverse: true}
+	]
+
+	const result = sorting(books, props)
 	const ids = result.map(book => book.id)
 
 	t.deepEqual(ids, [3, 1, 2, 4])
 })
 
 test('order by: Edition desc, author desc, title asc', t => {
-	const result = sort(books, {title: 'asc'})
+	const props = [
+		{name: 'edition', reverse: true},
+		{name: 'author', reverse: true},
+		{name: 'title'}
+	]
+
+	const result = sorting(books, props)
 	const ids = result.map(book => book.id)
 
 	t.deepEqual(ids, [3, 2, 4, 1])
 })
 
 test('order by: null', t => {
-	const error = t.throws(() => sort(books, null), Error)
+	const error = t.throws(() => sorting(books, null), Error)
 	t.is(error.message, 'Err')
 })
 
 test('order by: empty', t => {
-	const result = sort(books, {})
+	const result = sorting(books, [])
 	t.is(result.length, 0)
 })
